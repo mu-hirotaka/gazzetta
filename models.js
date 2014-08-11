1af0cff1a9882377dd981f2f0592f494a944a2de
hirop4 <hirop4@gmail.com>
2014-08-11 17:47:58 +0900

    Impl rating and opinion models

--- models.js
+++ models.js
@@ -10,3 +10,19 @@ var Player = new mongoose.Schema({
 });
 
 exports.Player = db.model('player', Player);
+
+var Rating = new mongoose.Schema({
+  id:  { type: Number },
+  num: { type: Number, default: 0 },
+  sum: { type: Number, default: 0 }
+});
+
+exports.Rating = db.model('rating', Rating);
+
+var Opinion = new mongoose.Schema({
+  id:        { type: Number },
+  opinion:   { type: String },
+  createdAt: { type: Number }
+});
+
+exports.Opinion = db.model('opinion', Opinion);
